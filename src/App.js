231ced10b2c81cc9a7c12ff64dd7356c10c40dfa
BhupendraYadav911 <Bhupendrayadav911@gmail.com>
2023-04-26 18:59:22 +0530

    working on Billing section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import Navbars from "./Navbar/Navbar";
-import HomeHeader from "./HomeHeader/HomeHeader";
+import Navbars from "./components/Navbar/Navbar";
+import HomeHeader from "./components/HomeHeader/HomeHeader"
+
+import BillingPayment from "./components/BillingPayment/BillingPayment";
+import Accelerate from "./components/Accelerate/Accelerate";
+import Serivces from "./components/serivces/Serivces";
 
 function App() {
   return (
     <>
      <Navbars />
      <HomeHeader />
+     <Accelerate />
+     <Serivces />
+     <BillingPayment />
     </>
   );
 }
---
+++ src/components/Accelerate/Accelerate.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Accelerate.css";
+
+const Accelerate = () => {
+  return (
+    <>
+      <section >
+        <div className="container-accelerate">
+          <center className="center-accelerate">
+            <h2>
+              Accelerate Digital Innovation with Our Custom <br />
+              Financial Application
+            </h2>
+            <h4>
+              We are dedicated to helping people digitize the way they pay, buy
+              and invest. We would be happy to share our passion as well as our
+              years of experience in bringing your business into the digital
+              life. We use the latest technologies including IOT, AI, and
+              Blockchain to develop user-friendly and secure{" "}
+              <b>fintech apps.</b>
+            </h4>
+          </center>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Accelerate;
